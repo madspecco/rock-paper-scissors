77c76563624762631c601c34a98bbf4dd6d08ec4
madspecco <bogdan.bolos30@gmail.com>
2022-08-17 17:27:22 +0300

    Optimized Functions (Need to create game() Function to play 5 rounds

--- script.js
+++ script.js
@@ -28,72 +28,74 @@ function getPlayerChoice(playerSelection) {
         return 0;
     }
 
-    else return 1;
+    else return choice;
 }
 
 function playRound(playerSelection, computerSelection) {
         playerSelection = getPlayerChoice();
-        //console.log("PLAYER SELECTS: " + playerSelection);
+        console.log("PLAYER SELECTS: " + playerSelection);
         computerSelection = getComputerChoice();
-        //console.log("COMPUTER SELECTS: " + computerSelection);
+        console.log("COMPUTER SELECTS: " + computerSelection);
 
         if(playerSelection === 'Rock') {
             if(computerSelection === 'Rock') {
-                //console.log("ISSA TIE! FEEL THE TENSION!");
+                console.log("ISSA TIE! FEEL THE TENSION!");
                 return "ISSA TIE! FEEL THE TENSION!"
             }
 
             if(computerSelection === 'Paper') {
-                //console.log("THE COMPUTER WINS! TAKE THAT HUMAN!");
+                console.log("THE COMPUTER WINS! TAKE THAT HUMAN!");
                 return "THE COMPUTER WINS! TAKE THAT HUMAN!";
             }
 
             if(computerSelection === 'Scissors') {
-                //console.log("THE PLAYER WINS!");
+                console.log("THE PLAYER WINS!");
                 return "THE PLAYER WINS!";
             }
         }
 
         if(playerSelection === 'Paper') {
             if(computerSelection === 'Rock') {
-                //console.log("THE PLAYER WINS!");
+                console.log("THE PLAYER WINS!");
                 return "THE PLAYER WINS!";
             }
 
             if(computerSelection === 'Paper') {
-                //console.log("ISSA TIE! FEEL THE TENSION!");
+                console.log("ISSA TIE! FEEL THE TENSION!");
                 return "ISSA TIE! FEEL THE TENSION!"
             }
 
             if(computerSelection === 'Scissors') {
-                //console.log("THE COMPUTER WINS! TAKE THAT HUMAN!");
+                console.log("THE COMPUTER WINS! TAKE THAT HUMAN!");
                 return "THE COMPUTER WINS! TAKE THAT HUMAN!";
             }
         }
 
         if(playerSelection === 'Scissors') {
             if(computerSelection === 'Rock') {
-                //console.log("THE COMPUTER WINS! TAKE THAT HUMAN!");
+                console.log("THE COMPUTER WINS! TAKE THAT HUMAN!");
                 return "THE COMPUTER WINS! TAKE THAT HUMAN!";
             }
 
             if(computerSelection === 'Paper') {
-                //console.log("THE PLAYER WINS!");
+                console.log("THE PLAYER WINS!");
                 return "THE PLAYER WINS!";
             }
 
             if(computerSelection === 'Scissors') {
-                //console.log("ISSA TIE! FEEL THE TENSION!");
+                console.log("ISSA TIE! FEEL THE TENSION!");
                 return "ISSA TIE! FEEL THE TENSION!"
             }
         }
     }
 
+
 // Create a function to play 5 rounds
 function game() {
     for(let i = 1; i <= 5; i++) {
-        playRound();
         console.log("ROUND " + i +" BEGINS:");
+        playRound();
+        console.log("END OF ROUND " + i);
     }
 }
 
