e8e22b02189bb470e13b95f02d622a196c9a89ec
madspecco <bogdan.bolos30@gmail.com>
2022-08-17 16:52:34 +0300

    Created playRound() Function to play ONE round

--- script.js
+++ script.js
@@ -17,8 +17,79 @@ function getComputerChoice(computerSelection) {
     }
 }
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let choice = prompt("Rock, Paper, Scissors?");
+    // make choice case sensitive
+    choice = choice[0].toUpperCase() + choice.slice(1, choice.length).toLowerCase();
+    
+    // failsafe
+    if(choice !== 'Rock' && choice !== 'Paper' && choice !== 'Scissors') {
+        alert("Whoops! Looks like that is not a valid choice.");
+    }
+
+    else {
+        console.log("ROUND BEGINS:");
+        playerSelection = choice;
+        //console.log("PLAYER SELECTS: " + playerSelection);
+        computerSelection = getComputerChoice();
+        //console.log("COMPUTER SELECTS: " + computerSelection);
+
+        if(playerSelection === 'Rock') {
+            if(computerSelection === 'Rock') {
+                //console.log("ISSA TIE! FEEL THE TENSION!");
+                return "ISSA TIE! FEEL THE TENSION!"
+            }
+
+            if(computerSelection === 'Paper') {
+                //console.log("THE COMPUTER WINS! TAKE THAT HUMAN!");
+                return "THE COMPUTER WINS! TAKE THAT HUMAN!";
+            }
+
+            if(computerSelection === 'Scissors') {
+                //console.log("THE PLAYER WINS!");
+                return "THE PLAYER WINS!";
+            }
+        }
+
+        if(playerSelection === 'Paper') {
+            if(computerSelection === 'Rock') {
+                //console.log("THE PLAYER WINS!");
+                return "THE PLAYER WINS!";
+            }
+
+            if(computerSelection === 'Paper') {
+                //console.log("ISSA TIE! FEEL THE TENSION!");
+                return "ISSA TIE! FEEL THE TENSION!"
+            }
+
+            if(computerSelection === 'Scissors') {
+                //console.log("THE COMPUTER WINS! TAKE THAT HUMAN!");
+                return "THE COMPUTER WINS! TAKE THAT HUMAN!";
+            }
+        }
+
+        if(playerSelection === 'Scissors') {
+            if(computerSelection === 'Rock') {
+                //console.log("THE COMPUTER WINS! TAKE THAT HUMAN!");
+                return "THE COMPUTER WINS! TAKE THAT HUMAN!";
+            }
+
+            if(computerSelection === 'Paper') {
+                //console.log("THE PLAYER WINS!");
+                return "THE PLAYER WINS!";
+            }
+
+            if(computerSelection === 'Scissors') {
+                //console.log("ISSA TIE! FEEL THE TENSION!");
+                return "ISSA TIE! FEEL THE TENSION!"
+            }
+        }
+    }
+
+}
+
+playRound();
+playRound();
+playRound();
+playRound();
+playRound();
