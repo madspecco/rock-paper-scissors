8b6587eb02b65c8b1e158cf4035ea57a712bcfd1
madspecco <bogdan.bolos30@gmail.com>
2022-08-26 11:33:44 +0300

    Added 3 Buttons that reflect the player's choice

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 /* begin with a function called getComputerChoice that
    will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’ */
 
+
+
+// (you can keep the console.logs for this step) 
+
 function getComputerChoice(computerSelection) {
     computerSelection = Math.ceil(Math.random() * 3);
 
@@ -17,24 +21,30 @@ function getComputerChoice(computerSelection) {
     }
 }
 
-function getPlayerChoice(playerSelection) {
-    let choice = prompt("Rock, Paper, Scissors?");
-    // make choice case sensitive
-    choice = choice[0].toUpperCase() + choice.slice(1, choice.length).toLowerCase();
-        
-    // failsafe
-    if(choice !== 'Rock' && choice !== 'Paper' && choice !== 'Scissors') {
-        alert("Whoops! Looks like that is not a valid choice.");
-        return 0;
-    }
+/* Create three buttons, one for each selection. */
+/* Add an event listener to the buttons that call your playRound function */
+/* with the correct playerSelection every time a button is clicked. */
 
-    else return choice;
-}
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+
+rock.addEventListener('click', function () {
+    playRound('Rock', getComputerChoice());
+});
+
+paper.addEventListener('click', function () {
+    playRound('Paper', getComputerChoice());
+});
+
+scissors.addEventListener('click', function () {
+    playRound('Scissors', getComputerChoice());
+});
+
 
 function playRound(playerSelection, computerSelection) {
-        playerSelection = getPlayerChoice();
         console.log("PLAYER SELECTS: " + playerSelection);
-        computerSelection = getComputerChoice();
         console.log("COMPUTER SELECTS: " + computerSelection);
 
         if(playerSelection === 'Rock') {
@@ -88,47 +98,46 @@ function playRound(playerSelection, computerSelection) {
             }
         }
     }
-
-
+    
 // Create a function to play 5 rounds
 
-function game() {
-    let player_score = 0;
-    let computer_score = 0;
+// function game() {
+//     let player_score = 0;
+//     let computer_score = 0;
 
-    for (let i = 1; i <= 5; i++) {
+//     for (let i = 1; i <= 5; i++) {
 
-        console.log("ROUND " + i + " BEGINS!");
-        let result = playRound();
+//         console.log("ROUND " + i + " BEGINS!");
+//         let result = playRound();
     
-        if(result === "THE PLAYER WINS!") {
-            player_score++;
-            console.log("PLAYER: " + player_score + " COMPUTER: " + computer_score);
-        }
+//         if(result === "THE PLAYER WINS!") {
+//             player_score++;
+//             console.log("PLAYER: " + player_score + " COMPUTER: " + computer_score);
+//         }
     
-        if(result === "THE COMPUTER WINS! TAKE THAT HUMAN!") {
-            computer_score++;
-            console.log("PLAYER: " + player_score + " COMPUTER: " + computer_score);
-        }
+//         if(result === "THE COMPUTER WINS! TAKE THAT HUMAN!") {
+//             computer_score++;
+//             console.log("PLAYER: " + player_score + " COMPUTER: " + computer_score);
+//         }
     
-        if(result === "ISSA TIE! FEEL THE TENSION!") {
-            console.log("PLAYER: " + player_score + " COMPUTER: " + computer_score);
-        }
+//         if(result === "ISSA TIE! FEEL THE TENSION!") {
+//             console.log("PLAYER: " + player_score + " COMPUTER: " + computer_score);
+//         }
     
-        console.log("ROUND " + i + " IS OVER!");
-    }
+//         console.log("ROUND " + i + " IS OVER!");
+//     }
 
-    if(player_score > computer_score) {
-        console.log("THE PLAYER WINS THE GAME :) !");
-    }
+//     if(player_score > computer_score) {
+//         console.log("THE PLAYER WINS THE GAME :) !");
+//     }
 
-    else if(player_score < computer_score) {
-        console.log("THE PLAYER LOST THE GAME :( !");
-    }
+//     else if(player_score < computer_score) {
+//         console.log("THE PLAYER LOST THE GAME :( !");
+//     }
 
-    else {
-        console.log("UNBELIEVABLE! IT'S A TIE!")
-    }
-}
+//     else {
+//         console.log("UNBELIEVABLE! IT'S A TIE!")
+//     }
+// }
 
-game();
+// game();
